435fe75e49647a7642d851129f74abca12845bd7
Bruno <brunoluancruzleal@gmail.com>
2026-02-10 16:46:26 -0300

    fist commit

---
+++ src/main/java/com/loja/model/ItemVenda.java
@@ -0,0 +1,14 @@
+package com.loja.model;
+
+import java.math.BigDecimal;
+
+public class ItemVenda {
+    private Long id;
+    private String nomeProduto;
+    private Integer quantidadeProduto;
+    private BigDecimal valorTotalItem;
+
+
+
+
+}
---
+++ src/main/java/com/loja/repository/VendaRepository.java
@@ -0,0 +1,9 @@
+package com.loja.repository;
+
+import com.loja.model.Venda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendaRepository extends JpaRepository<Venda,Long> {
+}
